87bc9e52e06f136aa4a20ff655961004977a0275
sriram <sriravisankar28@gmail.com>
2025-01-06 10:57:39 +0530

    array sorted

---
+++ chack array sorted/index.js
@@ -0,0 +1,13 @@
+const arr1 = [1, 2, 3, 1, 5];
+const arr = [1, 2, 3, 4, 5];
+function isSorted(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(
+    isSorted(arr)
+);
