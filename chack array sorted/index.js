2d6aa099127a9c0fb2a07281b30cdcca3a601b65
sriram <sriravisankar28@gmail.com>
2025-01-07 11:20:11 +0530

    remove duplicate from array

--- chack array sorted/index.js
+++ chack array sorted/index.js
@@ -1,7 +1,7 @@
 const arr1 = [1, 2, 3, 1, 5];
 const arr = [1, 2, 3, 4, 5];
 function isSorted(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         if (arr[i] > arr[i + 1]) {
             return false;
         }
---
+++ remove duplicate from array/index.js
@@ -0,0 +1,47 @@
+const arr = [1, 1, 2, 2, 2, 3, 3];
+
+// Brute force approach - Set()
+function bruteForce(arr) {
+    // create set
+    const set = new Set();
+    // loop array and add elements to set
+    for (let i = 0; i < arr.length; i++) {
+        set.add(arr[i]);
+    }
+    console.log(set);
+    // index to access array.
+    let index = 0;
+    // loop through set and replace elements in array
+    for (let element of set) {
+        console.log(element);
+        arr[index] = element;
+        index++
+    }
+    console.log(arr);
+
+}
+// bruteForce(arr);
+
+// optimal solution - two pointer.
+
+function twoPointer(arr) {
+    let left = 0;
+    let right = 1;
+    while (right < arr.length) {
+        if (arr[left] === arr[right]) {
+            right++;
+        }
+        else {
+            left++;
+            arr[left] = arr[right];
+            right++
+        }
+    }
+    // duplicates removed
+    console.log(arr);
+    // 3 elements changed
+    console.log(left + 1);
+
+
+}
+twoPointer(arr);
